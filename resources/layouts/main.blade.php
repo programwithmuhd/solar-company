691bd4aa61e3efcff31ce3b29f332e0a7f373d88
Muhammad Abubakar Muhammad <programwithmuhd@gmail.com>
2023-01-09 02:22:51 +0100

    fixed the search icon positioning class

--- resources/layouts/main.blade.php
+++ resources/layouts/main.blade.php
@@ -43,7 +43,7 @@
                         <input class="w-auto h-auto sm:w-auto md:w-auto lg:w-auto bg-gray-200 rounded-full px-8 py-1 mx-5 focus:outline-none focus:shadow-outline" type="text" name="query" placeholder="Search"
                         value="{{ request()->input('query') }}">
                     </form>
-                    <div class="absolute top-0u">
+                    <div class="absolute top-0">
                         <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                         </svg>
